50ea75b9e51c501a4e72abdd040c5fadd2b5710b
adeyemiiiii7 <aladesuyiadeyemi05@gmail.com>
2024-10-27 09:33:20 +0100

    Refactor user model to include additional fields for tracking user activity

--- server/models/user.js
+++ server/models/user.js
@@ -1,35 +1,59 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+  const { DataTypes } = require('sequelize');
+  const sequelize = require('../config/database');
 
-const User = sequelize.define('User', {
-  user_id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  role: {
-    type: DataTypes.ENUM('student', 'course_rep'),
-    allowNull: false,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  level: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-  },
-}, {
-  tableName: 'Users', 
-});
+  const User = sequelize.define('User', {
+    user_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    role: {
+      type: DataTypes.ENUM('student', 'course_rep'),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    level: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    last_active_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    current_streak: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    highest_streak: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+    total_active_days: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0,
+    },
+  }, {
+    tableName: 'Users',
+    timestamps: true,
+  });
 
-module.exports = User;
+  module.exports = User;
