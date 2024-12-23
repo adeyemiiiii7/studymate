b171e53e45760a54ef3324aa1ac7b59568f6bf0b
adeyemi <aladesuyiadeyemi05@gmail.com>
2024-12-23 13:24:31 -0800

    Add logging for database connection details in database.js

--- server/config/database.js
+++ server/config/database.js
@@ -1,4 +1,10 @@
-require('dotenv').config(); 
+require('dotenv').config();  
+
+console.log('DB Name:', process.env.DB_NAME);
+console.log('DB User:', process.env.DB_USER);
+console.log('DB Password:', process.env.DB_PASSWORD);
+console.log('DB Host:', process.env.DB_HOST);
+
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
