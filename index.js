95f4506feb8af0f25bdd67fe6878e9fe7b747d4e
adeyemi <aladesuyiadeyemi05@gmail.com>
2025-01-09 19:52:31 +0100

    Refactor server port configuration to use environment variable

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ sequelize
   })
   .catch(err => console.log('Error syncing database:', err));
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+ const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+  
